e61061eaaefc70d23d046fe2fecbb378fd4c0910
shilong <1490683763@qq.com>
2022-12-25 19:17:06 +0800

    模板入口

---
+++ src/compiler/index.js
@@ -0,0 +1,3 @@
+export function compileToFunction(template) {
+  console.log(template);
+}
--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
 import { initState } from "./state";
+import { compileToFunction } from "./compiler/index";
 
 export function initMixin(Vue) {
   Vue.prototype._init = function (options) {
@@ -10,5 +11,37 @@ export function initMixin(Vue) {
     initState(vm);
 
     // todo...
+    if (options.el) {
+      vm.$mount(options.el); // 实现数据挂载
+    }
+  };
+
+  Vue.prototype.$mount = function (el) {
+    const vm = this;
+    el = document.querySelector(el);
+    let ops = vm.$options;
+    // 先检查是否写了 render 函数
+    if (!ops.render) {
+      let template;
+      // 再检查是否写了 template，没写 template 使用 el 属性
+      if (!ops.template && el) {
+        template = el.outerHTML;
+      } else {
+        if (el) {
+          template = ops.template;
+        }
+      }
+      if (template) {
+        // 这里需要对模板进行编译
+        const render = compileToFunction(template);
+        ops.render = render;
+      } 
+      console.log("template", el, template);
+    }
+
+    // script 标签引用的 vue.global.js 这个编译过程是再浏览器运行的
+    // runtime 是不包含模板编译的，整个编译是再打包的时候通过 loader 来转义 .vue 文件的，用 runtime 的时候不能使用 template
+
+    ops.render;
   };
 }
